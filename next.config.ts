1d49d0fb4cd0548e7cc95cfea2d8809e5569b18e
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-10-24 09:54:58 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@g

--- next.config.ts
+++ next.config.ts
@@ -7,35 +7,4 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    // Exclude problematic files from yahoo-finance2
-    config.resolve.alias['yahoo-finance2/esm/tests/fetchCache.js'] = false;
-    config.resolve.alias['yahoo-finance2/esm/tests/setup.js'] = false;
-    return config;
-  },
-};
-
-export default nextConfig;
+...
