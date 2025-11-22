e5c18daebe82c6527ff4b9c89a200171abbc9015
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-11-22 14:25:27 +0000

    the logo must be https://lh3.googleusercontent.com//file/d/1kNohQtEjQUqe

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
   typescript: {
